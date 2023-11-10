a958c39f4a86c5655d0ce7fd9f981c52468b90eb
Ida Voong <idavoong@gmail.com>
2023-11-09 21:07:33 -0800

    first commit

---
+++ src/client.jsx
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const URL = 'https://ahskmpgfkbwuyujbmyzw.supabase.co';
+const API_KEY
+
+export const supabase = createClient(URL, API_KEY);
